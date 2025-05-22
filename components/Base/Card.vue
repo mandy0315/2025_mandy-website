ce18fa1fc1f6a124a565208dfccf11bb35fc70a9
mandy <mandy3152618@gmail.com>
2025-05-22 15:58:25 +0800

    fix: 刪除圖片元件直接在 Card 元件處理圖片

---
+++ components/Base/Card.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+const props = defineProps<{
+  path: string,
+  image: string,
+  date: string,
+  title: string,
+  description: string,
+}>()
+
+// 圖片處理
+const currSrc = ref(props.image);
+const handleErrorImage = () => {
+  const defaultImgSrc = '/images/default-image.jpg';
+  currSrc.value = defaultImgSrc;
+}
+
+</script>
+<template>
+  <NuxtLink :to="path"
+    class="bg-gray-100 dark:bg-gray-100/10 border c-border-gray shadow-lg rounded-lg overflow-hidden group">
+
+    <div class="w-full h-0 pb-[56.25%] relative rounded-lg overflow-hidden">
+      <img :src="currSrc" :alt="title"
+        class="h-full transition object-center object-cover w-full absolute duration-500 ease-in-out group-hover:blur-sm group-hover:scale-110"
+        @error="handleErrorImage" />
+    </div>
+    <div class="p-2">
+      <BaseDate :date />
+
+      <p class="text-xl"> {{ title }}</p>
+      <p>{{ description }}</p>
+    </div>
+  </NuxtLink>
+</template>
