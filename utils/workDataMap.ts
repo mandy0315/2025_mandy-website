8b6153d548f7e9524c1e24526ff0f686459427e5
mandy <mandy3152618@gmail.com>
2025-06-11 15:15:46 +0800

    feat: 新增 workItem

--- utils/workDataMap.ts
+++ utils/workDataMap.ts
@@ -3,8 +3,24 @@ import VisionData from "@/public/json/works/vision.json";
 import WebData from "@/public/json/works/web.json";
 
 type WorkDataKeys = "ui" | "vision" | "web";
+export interface WorkItem {
+  id: string;
+  /** string="ui" | "vision" | "web" */
+  category: string;
+  /** string="commercial" | "personal" */
+  type: string;
+  title: string;
+  description: string;
+  link: string | null;
+  image: string | null;
+  client: string | null;
+  skills: string[];
+  year: number | null;
+  /** string:"commercial" | "personal" */
+  status: string;
+}
 
-export const workDataMap = new Map<WorkDataKeys, any>([
+export const workDataMap = new Map<WorkDataKeys, WorkItem[]>([
   ["ui", UIData],
   ["vision", VisionData],
   ["web", WebData],
