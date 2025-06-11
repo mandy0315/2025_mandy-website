4fa4e208726ce3391de2721b265971f670351f13
mandy <mandy3152618@gmail.com>
2025-06-11 14:19:09 +0800

    chore: appMap 檔案改名

---
+++ utils/appMap.ts
@@ -0,0 +1,7 @@
+export const pageMap = new Map<string, { path: string; title: string }>([
+  ["index", { path: "/", title: "首頁" }],
+  ["posts", { path: "/posts", title: "文章" }],
+  ["categoriesPosts", { path: "/categories-posts", title: "文章分類" }],
+  ["notes", { path: "/notes", title: "筆記" }],
+  ["categoriesNotes", { path: "/categories-notes", title: "筆記分類" }],
+]);
