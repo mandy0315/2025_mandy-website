cd4c0c3622106d930e9828929089a18f17b8724c
mandy <mandy3152618@gmail.com>
2025-07-01 10:29:43 +0800

    refactor: 標題與說明改用 toRef 與 toValue

--- composables/usePageSEO.ts
+++ composables/usePageSEO.ts
@@ -11,9 +11,8 @@ export const usePageSEO = ({ title = "", description = "" }: UsePageSEO) => {
       ? descriptionInput
       : "Hello～我是 Mandy！歡迎來到我的小天地 ✨ 這裡收藏著我的開發與設計作品、學習筆記、生活與技術文章，希望能與你分享我的所見所想～";
   };
-
-  const fullTitle = computed(() => getTitle(title));
-  const fullDescription = computed(() => getDescription(description));
+  const fullTitle = toRef(() => getTitle(toValue(title)));
+  const fullDescription = toRef(() => getDescription(toValue(description)));
 
   useSeoMeta({
     title: fullTitle,
