445e1bb52e0d324fc288a4b904358bb4b18da2e7
mandy <mandy3152618@gmail.com>
2025-07-02 10:13:23 +0800

    docs: 頁面標題改英文

--- utils/appMap.ts
+++ utils/appMap.ts
@@ -1,6 +1,6 @@
 export const pageMap = new Map<string, { path: string; title: string }>([
-  ["index", { path: "/", title: "首頁" }],
-  ["works", { path: "/works", title: "作品" }],
-  ["posts", { path: "/posts", title: "文章" }],
-  ["notes", { path: "/notes", title: "筆記" }],
+  ["index", { path: "/", title: "home" }],
+  ["works", { path: "/works", title: "works" }],
+  ["posts", { path: "/posts", title: "posts" }],
+  ["notes", { path: "/notes", title: "notes" }],
 ]);
