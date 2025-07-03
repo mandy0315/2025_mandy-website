24cb79760731d86815f00db75a8b758cde7ce1e7
mandy <mandy3152618@gmail.com>
2025-07-03 14:10:00 +0800

    feat: 新增 menu icon

---
+++ components/App/Menu.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const isOpenModal = ref(false);
+</script>
+<template>
+  <button @click="isOpenModal = !isOpenModal" class="focus:outline-none">
+    <p class="text-[12px]">menu</p>
+    <div class="flex flex-col h-[20px] justify-between scale-75 sm:scale-80 lg:scale-100">
+      <div class="w-[32px] h-[1px] bg-black dark:bg-white transition-all origin-left"
+        :class="{ 'rotate-35 translate-x-[2px]': isOpenModal }"></div>
+      <div class="w-[32px] h-[1px] dark:bg-white bg-black transition-all origin-center"
+        :class="{ 'opacity-0': isOpenModal }"></div>
+      <div class="w-[32px] h-[1px] bg-black dark:bg-white transition-all origin-left"
+        :class="{ '-rotate-35 translate-x-[2px]': isOpenModal }"></div>
+    </div>
+  </button>
+
+</template>
