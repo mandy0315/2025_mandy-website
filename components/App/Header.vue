8c96d4e70093b52a6ca5423bef0be62dab693bab
mandy <mandy3152618@gmail.com>
2025-07-03 18:03:27 +0800

    feat: 新增 menu lightbox & style: header 桌機樣式調整

--- components/App/Header.vue
+++ components/App/Header.vue
@@ -7,16 +7,21 @@ const capitalize = (str: string) => {
 </script>
 <template>
   <header>
-    <NuxtLink to="/" class="fixed top-4 left-4 z-100">
+    <NuxtLink to="/" class="fixed top-4 left-4 z-80">
       Logo
     </NuxtLink>
-    <!-- bg-[var(--bg-color)]/80 -->
+    <AppMenu />
     <div
-      class="bg-amber-300 backdrop-blur-sm h-c-mobile-header-height flex border-b c-border-gray w-full fixed top-0 justify-between items-center z-50 flex-row-reverse px-4">
-      <AppMenu class="px-1.5" />
+      class="bg-[var(--bg-color)]/80 backdrop-blur-sm h-c-mobile-header-height flex w-full fixed top-0 justify-between items-center z-50 flex-row-reverse pl-4 pr-10 lg:h-screen lg:right-0 lg:w-c-web-header-width lg:flex-col lg:px-0 lg:pt-10 lg:pb-4">
       <AppSearch class="px-1.5" />
-      <AppColorModel class="ml-auto" />
+      <p class="hidden lg:block lg:text-xs vertical-rl">@2025mandyspace.</p>
+      <AppColorModel class="ml-auto lg:ml-0" />
     </div>
   </header>
 
 </template>
+<style scoped>
+.vertical-rl {
+  writing-mode: vertical-rl;
+}
+</style>
--- components/App/Menu.vue
+++ components/App/Menu.vue
@@ -2,16 +2,22 @@
 const isOpenModal = ref(false);
 </script>
 <template>
-  <button @click="isOpenModal = !isOpenModal" class="focus:outline-none">
-    <p class="text-[12px]">menu</p>
-    <div class="flex flex-col h-[20px] justify-between scale-75 sm:scale-80 lg:scale-100">
-      <div class="w-[32px] h-[1px] bg-black dark:bg-white transition-all origin-left"
-        :class="{ 'rotate-35 translate-x-[2px]': isOpenModal }"></div>
-      <div class="w-[32px] h-[1px] dark:bg-white bg-black transition-all origin-center"
-        :class="{ 'opacity-0': isOpenModal }"></div>
-      <div class="w-[32px] h-[1px] bg-black dark:bg-white transition-all origin-left"
-        :class="{ '-rotate-35 translate-x-[2px]': isOpenModal }"></div>
+  <div>
+    <button @click="isOpenModal = !isOpenModal"
+      class="focus:outline-none  fixed top-1.5 right-2 z-100 scale-75 sm:scale-100 sm:right-3 sm:top-3 lg:right-3.5">
+      <p class="text-[12px]">menu</p>
+      <div class="flex flex-col h-[20px] justify-between">
+        <div class="w-[32px] h-[1px] bg-black dark:bg-white transition-all origin-left"
+          :class="{ 'rotate-35 translate-x-[2px]': isOpenModal }"></div>
+        <div class="w-[32px] h-[1px] dark:bg-white bg-black transition-all origin-center"
+          :class="{ 'opacity-0': isOpenModal }"></div>
+        <div class="w-[32px] h-[1px] bg-black dark:bg-white transition-all origin-left"
+          :class="{ '-rotate-35 translate-x-[2px]': isOpenModal }"></div>
+      </div>
+    </button>
+    <div v-if="isOpenModal" class="fixed z-80 top-0 left-0 w-full h-full bg-white ">
+      menu
     </div>
-  </button>
+  </div>
 
 </template>
