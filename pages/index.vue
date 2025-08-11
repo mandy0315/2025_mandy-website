92e0c2a4c97ef613eb87c776926c7a8433b44f17
mandy <mandy3152618@gmail.com>
2025-08-11 14:55:40 +0800

    style: 首頁切版

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,31 @@
 usePageSEO({
   title: '首頁',
 })
+
+const kvDirectionsRef = ref<HTMLElement | null>(null);
 </script>
 
 <template>
-  <div class="text-c-light-blue">首頁- nuxt3 contant 練習</div>
+  <section
+    class="flex flex-col justify-center w-8/10 min-h-[30vh] mx-auto lg:w-4/10 lg:min-h-screen lg:pl-16 lg:mx-0 lg:mr-auto">
+    <h1>kv</h1>
+    <p ref="kvDirectionsRef">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo quia inventore deserunt sint
+      quaerat
+      praesentium,
+      blanditiis nam unde. Repellendus et sapiente voluptates laborum omnis earum ullam quaerat, molestiae neque
+      laudantium.</p>
+  </section>
+  <div
+    class="w-full min-h-[65vh] bg-c-image-gray lg:fixed lg:top-0 lg:right-[var(--spacing-c-web-header-width)] lg:m-h-auto lg:h-screen lg:w-5/10 lg:z-10">
+    image
+  </div>
+  <section class="py-40">
+    <h2>profile</h2>
+  </section>
+  <section class="py-40 bg-[var(--bg-color)] relative z-20">
+    <h2>topics</h2>
+  </section>
+  <section class="py-40 bg-[var(--bg-color)] relative z-20">
+    <h2>works</h2>
+  </section>
 </template>
