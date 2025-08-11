34a8a10e5840443d2ce9e3962bf1c840148d59bb
mandy <mandy3152618@gmail.com>
2025-08-11 15:24:10 +0800

    fix: posts 路徑改 blog

--- utils/appMap.ts
+++ utils/appMap.ts
@@ -1,6 +1,6 @@
 export const pageMap = new Map<string, { path: string; title: string }>([
   ["index", { path: "/", title: "home" }],
   ["works", { path: "/works", title: "works" }],
-  ["posts", { path: "/posts", title: "posts" }],
+  ["blog", { path: "/blog", title: "blog" }],
   ["notes", { path: "/notes", title: "notes" }],
 ]);
