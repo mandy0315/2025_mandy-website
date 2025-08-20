3c6cb4c146c6968f73111edc720cb883252b937d
mandy <mandy3152618@gmail.com>
2025-08-20 11:30:28 +0800

    fix: 側邊欄樣式與 cookie

--- layouts/ContentWithSidebar.vue
+++ layouts/ContentWithSidebar.vue
@@ -1,36 +1,35 @@
 <script setup lang="ts">
-const isShowSidebarCookie = useCookie('isShowSidebar', {
-  default: () => 'false',
-  maxAge: 60 * 60 * 24 * 7, // 設定 7 天
+const isShowSidebar = useCookie('isShowSidebar', {
+  default: () => false,
   path: '/',
   watch: true,
 });
-const isShowSidebar = useState('isShowSidebar', () => Boolean(isShowSidebarCookie.value));
 const toggleSidebar = () => {
   isShowSidebar.value = !isShowSidebar.value;
-  isShowSidebarCookie.value = String(isShowSidebar.value);
+  refreshCookie('isShowSidebar');
 }
 
 </script>
 <template>
   <div class="flex">
-    <div class="pb-10 relative w-full">
-      <BaseButton class="sticky top-0 left-full z-50" @click="toggleSidebar">
-        <ClientOnly>
-          <Icon v-if="isShowSidebar" name="solar:double-alt-arrow-right-line-duotone" size="1.5rem"
+    <ClientOnly>
+      <div class="pb-10 relative w-full">
+        <BaseButton size="sm" variant="solid" class="sticky rounded-l-3xl rounded-r-none top-15 left-full z-10"
+          @click="toggleSidebar">
+
+          <Icon v-if="isShowSidebar" name="solar:double-alt-arrow-right-line-duotone" size="1rem"
             class="align-middle" />
           <span v-else>
-            <Icon name="solar:double-alt-arrow-left-line-duotone" size="1.5rem" class="align-middle" />
+            <Icon name="solar:double-alt-arrow-left-line-duotone" size="1rem" class="align-middle" />
             <span class="align-middle pr-1 text-xs">側邊欄</span>
           </span>
-        </ClientOnly>
-      </BaseButton>
-      <div class="pr-4">
-        <slot name="content" />
+        </BaseButton>
+        <div class="pr-4">
+          <slot name="content" />
+        </div>
       </div>
-    </div>
-    <ClientOnly>
-      <div class="transition-all ease-in delay-150" :class="isShowSidebar ? 'w-60' : 'w-0'">
+      <div class="transition-all ease-in delay-150 rounded-l bg-primary/10 h-screen"
+        :class="isShowSidebar ? 'w-70' : 'w-0'">
         <!-- 右側目錄 -->
         <aside class="sticky transition-all pt-10 px-4" :class="isShowSidebar ? 'opacity-100 delay-300' : 'opacity-0'">
           <slot name="right-side" />
