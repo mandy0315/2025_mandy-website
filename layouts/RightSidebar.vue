a5ac21712d17774b72a52be518f7e01ece6e498a
mandy <mandy3152618@gmail.com>
2025-08-20 17:45:36 +0800

    style: 右側固定修改樣式

--- layouts/RightSidebar.vue
+++ layouts/RightSidebar.vue
@@ -11,28 +11,30 @@ const toggleSidebar = () => {
 
 </script>
 <template>
-  <div class="flex">
+  <div class="flex relative">
     <ClientOnly>
-      <div class="w-full pr-4">
+      <div class="flex-1 pr-4 min-h-screen">
         <slot name="content" />
       </div>
 
-      <div class="transition-all ease-in delay-150 rounded-t-full bg-primary/10 h-screen relative"
-        :class="isShowSidebar ? 'w-70' : 'w-0'">
-        <BaseButton size="sm" variant="solid" class=" absolute  rounded-l-3xl rounded-r-none top-24 right-full z-10"
-          @click="toggleSidebar">
+      <div class="w-0 relative transition-all ease-in-out duration-300" :class="isShowSidebar ? 'w-70' : 'w-0'">
+        <div class="rounded-t-full bg-primary/10 h-screen sticky top-10">
+          <BaseButton size="sm" variant="solid" class="absolute rounded-l-3xl rounded-r-none top-30 right-full z-50"
+            @click="toggleSidebar">
 
-          <Icon v-if="isShowSidebar" name="solar:double-alt-arrow-right-line-duotone" size="1rem"
-            class="align-middle" />
-          <span v-else class="text-nowrap">
-            <Icon name="solar:double-alt-arrow-left-line-duotone" size="1rem" class="align-middle" />
-            <span class="align-middle pr-1 text-xs">側邊欄</span>
-          </span>
-        </BaseButton>
-        <!-- 右側目錄 -->
-        <aside class="sticky transition-all pt-24 px-4" :class="isShowSidebar ? 'opacity-100 delay-300' : 'opacity-0'">
-          <slot name="right-side" />
-        </aside>
+            <Icon v-if="isShowSidebar" name="solar:double-alt-arrow-right-line-duotone" class="align-middle"
+              size="1rem" />
+            <span v-else class="text-nowrap">
+              <Icon name="solar:double-alt-arrow-left-line-duotone" size="1rem" class="align-middle" />
+              <span class="align-middle pr-1 text-xs">側邊欄</span>
+            </span>
+          </BaseButton>
+
+          <aside class="h-full overflow-y-auto transition-opacity duration-300 pt-24 px-4"
+            :class="isShowSidebar ? 'opacity-100 delay-300' : 'opacity-0'">
+            <slot name="right-side" />
+          </aside>
+        </div>
       </div>
     </ClientOnly>
   </div>
