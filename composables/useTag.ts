f2d09f98ce388357664b4a8378802e9e7ac990da
mandy <mandy3152618@gmail.com>
2025-08-21 18:34:29 +0800

    feat: 使用標籤方法

---
+++ composables/useTag.ts
@@ -0,0 +1,52 @@
+export const useTag = async (collection: "blog" | "notes" = "blog") => {
+  const tags = useState<string[]>(`tags-${collection}`, () => []);
+
+  const goToTagsPage = async (tag: string) => {
+    await navigateTo(`/${collection}/tags/${tag}`);
+  };
+
+  const { data: tagsData, refresh } = await useAsyncData(
+    `${collection}-tag-list`,
+    async () => {
+      return await queryCollection(collection)
+        .order("date", "DESC")
+        .select("tags")
+        .all();
+    }
+  );
+
+  const setTags = (limit?: number) => {
+    if (!tagsData.value) {
+      tagsData.value = [];
+      tags.value = [];
+      return;
+    }
+
+    let selectedTags: string[] = tagsData.value.map((item) => item.tags).flat(); // 取得物件的 category 欄位的值並扁平化陣列
+    selectedTags = Array.from(new Set(selectedTags)); // 篩選掉重複的
+
+    if (limit) {
+      selectedTags = selectedTags.slice(0, limit);
+    }
+
+    tags.value = selectedTags;
+  };
+
+  const refreshTags = async (limit?: number) => {
+    tags.value = [];
+    try {
+      await refresh();
+
+      setTags(limit);
+    } catch (error) {
+      console.error("取得標籤錯誤", error);
+    }
+  };
+
+  return {
+    goToTagsPage,
+    refreshTags,
+    setTags,
+    tags,
+  };
+};
