14cb25dffb1ec213e5d747f76cf0621fb714a16f
mandy <mandy3152618@gmail.com>
2025-08-21 18:31:31 +0800

    refactor: 命名修改

--- middleware/categories.ts
+++ middleware/categories.ts
@@ -12,15 +12,15 @@ export default defineNuxtRouteMiddleware(async (to, _) => {
   const { setCategories, categories } = await useCategory(collection);
   setCategories();
 
-  const category = String(to.params?.category || "");
+  const categoryParam = String(to.params?.category || "");
   // 沒有 category 並有分類列表，重定向到第一個分類
-  if (category === "" && categories.value.length > 0) {
+  if (categoryParam === "" && categories.value.length > 0) {
     return navigateTo(`/${collection}/categories/${categories.value[0]}`, {
       replace: true, // 使用 replace 以避免在歷史記錄中留下重定向
     });
   }
   // 分類裡沒有該分類
-  if (category && !categories.value.includes(category)) {
+  if (categoryParam && !categories.value.includes(categoryParam)) {
     throw createError({
       statusCode: 404,
       statusMessage: "Category Not Found",
