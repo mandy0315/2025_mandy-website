a39a6226dc7a6308066d2addfaebd6eeb6119a47
mandy <mandy3152618@gmail.com>
2025-08-22 11:47:50 +0800

    fix: 改 tag 路徑

--- middleware/tags.ts
+++ middleware/tags.ts
@@ -15,7 +15,7 @@ export default defineNuxtRouteMiddleware(async (to, _) => {
   const tagParam = String(to.params?.tag || "");
   // 網址沒有標籤顯示但有標籤列表，重定向到第一個標籤
   if (tagParam === "" && tags.value.length > 0) {
-    return navigateTo(`/${collection}/categories/${tags.value[0]}`, {
+    return navigateTo(`/${collection}/tags/${tags.value[0]}`, {
       replace: true, // 使用 replace 以避免在歷史記錄中留下重定向
     });
   }
