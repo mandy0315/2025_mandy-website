139931c2e39ad1e39a992d798c78df81792489e1
mandy <mandy3152618@gmail.com>
2025-08-22 14:31:57 +0800

    style: 架構修正

--- components/Post/Categories/Aside.vue
+++ components/Post/Categories/Aside.vue
@@ -4,11 +4,13 @@ const props = defineProps<{
 }>();
 
 const { categories, goToCategoriesPage } = await useCategory(props.collection);
+const route = useRoute();
 
 const LIMIT_COUNT = 10;
 const currentCount = ref(0);
 currentCount.value = LIMIT_COUNT;
 
+const currentCategory = computed(() => (route.params?.category || '') as string);
 const currentCategories = computed(() =>
   categories.value.slice(0, currentCount.value)
 );
@@ -27,27 +29,33 @@ const handleToggle = () => {
 </script>
 
 <template>
-  <div class="h-full overflow-y-auto">
-    <BaseLink to="/blog/categories" size="xs" class="rounded-t-sm rounded-b-none border-b-0 mr-1"
-      :isAction="collection === 'blog'">
-      部落格分類</BaseLink>
-    <BaseLink to="/notes/categories" size="xs" class="rounded-t-sm rounded-b-none border-b-0"
-      :isAction="collection === 'notes'">
-      筆記分類</BaseLink>
-    <div class="border-b c-border-secondary border-dashed "></div>
-    <ul class="py-2">
-      <li v-for="category in currentCategories" :key="category">
-        <button size="sm" class="py-1 px-2 block w-full text-sm text-left my-1 hover:bg-primary/30"
-          @click="goToCategoriesPage(category)">
-          <Icon name="solar:folder-bold" size="1rem" class=" align-middle" />
-          {{ category }}
-        </button>
-      </li>
-    </ul>
+  <div class="h-full pr-4">
+    <div class="h-8">
+      <BaseLink to="/blog/categories" size="xs" class="rounded-t-sm rounded-b-none border-b-0 mr-1"
+        :isAction="collection === 'blog'">
+        部落格分類</BaseLink>
+      <BaseLink to="/notes/categories" size="xs" class="rounded-t-sm rounded-b-none border-b-0"
+        :isAction="collection === 'notes'">
+        筆記分類</BaseLink>
+      <div class="border-b c-border-secondary border-dashed"></div>
+    </div>
 
-    <BaseButton v-if="isToggleButton" size="sm" class="w-full rounded-sm mb-2" @click="handleToggle">
-      {{ hasMore ? '顯示更多' : '收起' }}
-    </BaseButton>
-    <div class="border-b c-border-secondary border-dashed "></div>
+    <div class="overflow-y-auto h-[calc(100%-2rem)]">
+      <ul>
+        <li v-for="category in currentCategories" :key="category">
+          <button size="sm" class="py-1 px-2 block w-full text-sm text-left my-1 hover:bg-primary/30 "
+            :class="{ 'text-primary hover:bg-transparent': currentCategory === category }"
+            @click="goToCategoriesPage(category)">
+            <Icon name="solar:folder-bold" size="1rem" class=" align-middle" />
+            {{ category }}
+          </button>
+        </li>
+      </ul>
+
+      <BaseButton v-if="isToggleButton" size="sm" class="w-full rounded-sm mb-2" @click="handleToggle">
+        {{ hasMore ? '顯示更多' : '收起' }}
+      </BaseButton>
+      <div class="border-b c-border-secondary border-dashed "></div>
+    </div>
   </div>
 </template>
