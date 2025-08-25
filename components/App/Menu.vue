43136b95ec536ec040fe19fea3d32910cf2ee720
mandy <mandy3152618@gmail.com>
2025-08-25 17:25:24 +0800

    fix: firstWordToUpper 共用方法

--- components/App/Menu.vue
+++ components/App/Menu.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { pageInfo } from "@/utils/pageInfoMap";
+import { firstWordToUpper } from "@/utils/formatText";
 
 const route = useRoute()
 const isOpenMenu = useState('isOpenMenu', () => false);
@@ -25,13 +26,8 @@ const closeMenu = () => {
 
 const routeName = computed(() => (route.name || '') as string);
 
-const firstWordToUpperCase = (words: string) => {
-  if (words.length < 1) return words;
-  return words.charAt(0).toUpperCase() + words.slice(1);
-};
-
 const getChildName = (words: string) => {
-  return firstWordToUpperCase(words.split('-')[1] || words);
+  return firstWordToUpper(words.split('-')[1] || words);
 };
 </script>
 <template>
@@ -52,14 +48,14 @@ const getChildName = (words: string) => {
       <div
         class="fixed flex items-center justify-start z-100 top-0 left-0 h-full w-full transition-transform duration-700  flex-row pt-0"
         :class="isOpenMenu ? 'translate-x-0' : 'translate-x-full'">
-        <button @click="isOpenMenu = false" class="w-7/10 bg-[var(--bg-color)]/40 backdrop-blur-sm h-screen"></button>
+        <div @click.stop="isOpenMenu = false" class="w-7/10 bg-[var(--bg-color)]/40 backdrop-blur-sm h-screen"></div>
         <nav class="w-3/10 bg-[var(--bg-color)] h-screen flex  justify-center items-center flex-col">
           <div v-for="item in pageInfo.values()" :key="item.title"
             class="w-8/10 text-left mx-auto py-3 font-bold font-zen-old-mincho">
             <div class="flex">
               <NuxtLink :to="item.path" @click="closeMenu" class=" cursor-pointer hover:text-primary"
                 :class="{ 'text-primary': routeName.includes(item.name) }">
-                {{ firstWordToUpperCase(item.name) }} {{ item.title }}
+                {{ firstWordToUpper(item.name) }} {{ item.title }}
               </NuxtLink>
               <button v-if="item.childrens" @click="toggleSubMenu(item.title)"
                 class="text-lg px-3 transition-transform origin-center align-middle cursor-pointer ml-auto hover:text-primary"
