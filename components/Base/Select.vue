606a6b364c4a6628a232beb32875b48923d62c78
mandy <mandy3152618@gmail.com>
2025-08-28 10:16:54 +0800

    fix: 移出變成共用

---
+++ components/Base/Select.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    labelTitle?: string;
+    options: { value: string; label: string }[];
+  }>(),
+  {
+    labelTitle: '',
+  }
+);
+const value = defineModel<string>({
+  required: true
+})
+</script>
+<template>
+  <label class="inline-block text-sm text-secondary-light dark:text-secondary-dark select-none">
+    <span class="pr-2">{{ labelTitle }}</span>
+    <select v-model="value" class="border c-border-secondary rounded py-1 px-2 ">
+      <option v-for="(option, index) in options" :key="index" :value="option.value">{{ option.label }}</option>
+    </select>
+  </label>
+</template>
