6a8a76e85d2014d9c009597225085625cd6ea307
mandy <mandy3152618@gmail.com>
2025-11-11 18:24:47 +0800

    style: 文章卡片水平樣式調整

--- app/components/Post/Card.vue
+++ app/components/Post/Card.vue
@@ -18,18 +18,19 @@ const processedSrc = computed(() => getAssetPath(props.image));
 </script>
 <template>
   <NuxtLink :to="path" class="group">
-    <div class="grid" :class="isHorizontal ? 'grid-cols-3' : 'grid-cols-1'">
+    <div class="grid" :class="isHorizontal ? 'grid-cols-7' : 'grid-cols-1'">
       <!-- 圖片比例 16:9 -->
-      <div class="w-full h-0 pb-[56.25%] relative rounded-lg overflow-hidden col-span-1">
+      <div class="w-full h-0 pb-[56.25%] relative rounded-lg overflow-hidden "
+        :class="isHorizontal ? 'col-span-3' : 'col-span-1'">
         <BaseHoverMask contentText="進入文章" />
         <span class="absolute top-2 z-10 text-xs bg-primary px-1 text-white">{{ category }}</span>
         <img :src="processedSrc" :alt="title"
           class="h-full transition object-center object-cover w-full absolute duration-500 ease-in-out group-hover:blur-sm" />
       </div>
-      <div :class="isHorizontal ? 'flex flex-col justify-center px-6 col-span-2' : 'p-2 col-span-1'">
+      <div :class="isHorizontal ? 'flex flex-col px-6 py-4 col-span-4' : 'p-2 col-span-1'">
         <PostDate :date />
         <p class="text-xl"> {{ title }}</p>
-        <p class="c-text-secondary" :class="isHorizontal ? 'line-clamp-4' : 'line-clamp-2'">{{
+        <p class="c-text-secondary" :class="isHorizontal ? 'line-clamp-3' : 'line-clamp-2'">{{
           description }}</p>
       </div>
     </div>
