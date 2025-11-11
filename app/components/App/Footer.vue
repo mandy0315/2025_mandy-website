11d498ae4af4ee69a91a0172f78d848cc48357be
mandy <mandy3152618@gmail.com>
2025-11-11 16:21:18 +0800

    style: footer 響應式樣式

--- app/components/App/Footer.vue
+++ app/components/App/Footer.vue
@@ -12,25 +12,29 @@ onMounted(() => {
 });
 </script>
 <template>
-  <footer ref="footerRef" class="bg-[var(--bg-color)]/50 relative z-50">
-    <div class="c-container flex justify-between items-center py-2">
-      <div>
+  <footer ref="footerRef" class="bg-[var(--bg-color)] relative z-50 py-10 lg:py-6">
+    <div class="c-container flex flex-col lg:flex-row lg:justify-between items-center py-2">
+      <div class="pb-4">
         <IconLogoHorizontal class="dark:fill-white w-36" />
         <div class="pt-2">
-          <p class="c-text-secondarys text-xs pl-1 pb-2">
-            <em>嗨～我是蔓蒂<br />可以透過以下方式聯絡我：</em>
+          <p class="c-text-secondarys text-xs pl-1 pb-2 text-center lg:text-left">
+            <em><span class="hidden lg:block">嗨～我是蔓蒂</span>可以透過以下方式聯絡我：</em>
           </p>
-          <div class="flex gap-x-1">
+          <div class="flex gap-x-1 justify-center lg:justify-start">
             <BaseIconLink icon="mdi:email" to="mailto:mandy3152618@gmail.com" external />
             <BaseIconLink icon="mdi:facebook" to="https://www.facebook.com/MandyTsai0315" target="_blank" external />
             <BaseIconLink icon="mdi:github" to="https://github.com/mandy0315" target="_blank" external />
           </div>
         </div>
       </div>
-      <div>
-        <BaseLink v-for="item in pageInfo.values()" variant="underline" :to="item.path" class="text-xs no-underline">{{
-          item.title }}
-        </BaseLink>
+      <div class="lg:mt-auto">
+        <div class="lg:pb-2">
+          <BaseLink v-for="item in pageInfo.values()" variant="underline" :to="item.path" class="text-xs no-underline">
+            {{
+              item.title }}
+          </BaseLink>
+        </div>
+        <p class="text-xs text-center lg:text-right">©2024-2025 MandySpace.</p>
       </div>
     </div>
   </footer>
