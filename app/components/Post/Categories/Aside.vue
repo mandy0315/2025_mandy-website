c23be475f3bad093015817869925fe1c5123e0d8
mandy <mandy3152618@gmail.com>
2025-11-13 15:39:10 +0800

    style: 分類總覽側邊欄響應式的樣式

--- app/components/Post/Categories/Aside.vue
+++ app/components/Post/Categories/Aside.vue
@@ -5,33 +5,49 @@ const props = defineProps<{
 
 const { categories, goToCategoriesPage } = await useCategory(props.collection);
 const route = useRoute();
-
+const { isDesktop } = useResponsive();
+const isExpanded = ref(false);
 
 const currentCategory = computed(() => (route.params?.category || '') as string);
+const isShowSidebar = computed(() => isDesktop.value ? true : isExpanded.value)
 
 const { initScrollToTarget, containerRef, targetRefs } = useScrollToTarget(currentCategory);
 onMounted(async () => {
   await initScrollToTarget();
 })
+
 </script>
 
 <template>
-  <div class="h-full pr-4">
-    <div class="h-8">
-      <BaseLink to="/blog/categories" size="xs" class="rounded-t-sm rounded-b-none border-b-0 mr-1"
-        :isAction="collection === 'blog'">
-        部落格分類</BaseLink>
-      <BaseLink to="/notes/categories" size="xs" class="rounded-t-sm rounded-b-none border-b-0"
-        :isAction="collection === 'notes'">
-        筆記分類</BaseLink>
-      <div class="border-b c-border-secondary border-dashed"></div>
+  <div class="h-full border-y c-border-secondary lg:border-none">
+    <button @click="isExpanded = !isExpanded" class="py-2 px-4 flex items-center w-full lg:hidden">
+      <p>
+        <Icon name="i-solar:hamburger-menu-bold" size="1.2rem" class="align-middle" />
+        {{ collection === 'blog' ? '部落格分類' : '筆記分類' }}
+      </p>
+      <p class="text-lg ml-auto px-3 transition-transform origin-center align-middle cursor-pointer"
+        :class="{ 'rotate-45': isExpanded }">
+        +
+      </p>
+    </button>
+    <div v-if="isShowSidebar" class="bg-primary/10 py-2 px-3 lg:bg-transparent lg:pl-0 lg:py-0 lg:pr-4 lg:h-full">
+      <div v-if="isDesktop" class="h-8">
+        <BaseLink to="/blog/categories" size="xs" class="rounded-t-sm rounded-b-none border-b-0 mr-1"
+          :isAction="collection === 'blog'">
+          部落格分類</BaseLink>
+        <BaseLink to="/notes/categories" size="xs" class="rounded-t-sm rounded-b-none border-b-0"
+          :isAction="collection === 'notes'">
+          筆記分類</BaseLink>
+        <div v-if="isDesktop" class="border-b c-border-secondary border-dashed"></div>
+      </div>
+      <ul ref="containerRef"
+        class="grid grid-cols-2 gap-1 md:grid-cols-3 lg:block lg:overflow-y-auto lg:h-[calc(100%-2rem)]">
+        <li ref="targetRefs" v-for="category in categories" :key="category">
+          <PostCategoryButton :category :isAction="currentCategory === category"
+            @click="goToCategoriesPage(category)" />
+        </li>
+        <li v-if="isDesktop" class="border-b c-border-secondary border-dashed"></li>
+      </ul>
     </div>
-
-    <ul ref="containerRef" class="overflow-y-auto h-[calc(100%-2rem)]">
-      <li ref="targetRefs" v-for="category in categories" :key="category">
-        <PostCategoryButton :category :isAction="currentCategory === category" @click="goToCategoriesPage(category)" />
-      </li>
-      <li class="border-b c-border-secondary border-dashed"></li>
-    </ul>
   </div>
 </template>
