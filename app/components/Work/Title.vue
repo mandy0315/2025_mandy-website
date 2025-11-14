0dc48e133d257f67c9ca9f34929696c069801491
mandy <mandy3152618@gmail.com>
2025-11-14 17:54:41 +0800

    feat: 作品內頁標題元件

---
+++ app/components/Work/Title.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const props = defineProps<{
+  title: string;
+  subtitle: string;
+}>()
+</script>
+<template>
+  <h2 class="font-bold font-zen-old-mincho text-lg text-text-light dark:text-text-dark pb-2">
+    <span class="pr-1">{{ props.title }}</span>
+    <span class="text-base">{{ props.subtitle }}</span>
+  </h2>
+</template>
