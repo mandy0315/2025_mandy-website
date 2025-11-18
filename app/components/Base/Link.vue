fbd611d3fff67e3aa8098d3e0dac0f55e7e8761e
mandy <mandy3152618@gmail.com>
2025-11-18 11:59:30 +0800

    fix: 連結元件樣式調整

--- app/components/Base/Link.vue
+++ app/components/Base/Link.vue
@@ -4,15 +4,14 @@ const props = withDefaults(
     to: string;
     target?: '_self' | '_blank';
     external?: boolean;
-    variant?: 'outline' | 'solid' | 'underline';
+    variant?: 'outline' | 'solid' | 'underline' | 'base';
     size?: 'xs' | 'sm' | 'md' | 'lg';
     isAction?: boolean;
   }>(),
   {
     external: false,
     target: '_self',
-    variant: 'outline',
-    size: 'md',
+    variant: 'base',
     isAction: false,
   }
 );
@@ -20,33 +19,38 @@ const props = withDefaults(
 const linkClasses = computed(() => {
   const base = 'inline-block transition-colors disabled:opacity-40 disabled:cursor-not-allowed';
 
-  const textSizeClasses = {
+  const sizeClasses = props.size ? {
     xs: 'text-xs',
     sm: 'text-sm',
     md: 'text-base',
     lg: 'text-lg'
-  };
+  }[props.size] : '';
 
-  const paddingClasses = {
+  // 只有 outline 和 solid 需要 padding
+  const paddingClasses = props.size && (props.variant === 'outline' || props.variant === 'solid') ? {
     xs: 'px-2 py-1',
     sm: 'px-3 py-1.5',
     md: 'px-4 py-2',
     lg: 'px-6 py-3'
-  };
+  }[props.size] : '';
 
   const variantClasses = {
     outline: 'border border-primary text-primary bg-white/20 hover:bg-primary/70 hover:text-white active:bg-primary active:text-white rounded-4xl',
     solid: 'bg-primary/70 text-white hover:bg-primary-dark active:bg-primary active:text-white rounded-4xl',
     underline: 'underline underline-offset-3 hover:text-primary/70 active:text-primary',
+    base: 'hover:text-primary/70 active:text-primary',
   }
-
-  // 如果 isAction 為 true，強制使用 solid 樣式
-  const finalVariant = props.isAction ? 'solid' : props.variant;
+  const actionVariantClasses = {
+    outline: variantClasses.solid,
+    solid: variantClasses.solid,
+    underline: 'text-primary underline underline-offset-3',
+    base: 'text-primary',
+  }
   return [
     base,
-    props.variant === 'underline' ? '' : paddingClasses[props.size],
-    textSizeClasses[props.size],
-    variantClasses[finalVariant],
+    sizeClasses,
+    paddingClasses,
+    props.isAction ? actionVariantClasses[props.variant] : variantClasses[props.variant],
   ].join(' ');
 });
 
