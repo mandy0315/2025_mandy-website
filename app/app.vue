b7123fb70d3a97c9274944a4a95b6f2458001739
mandy <mandy3152618@gmail.com>
2025-11-28 17:49:18 +0800

    feat: 新增預設 layout

--- app/app.vue
+++ app/app.vue
@@ -24,14 +24,8 @@ useHead({
 <template>
   <Body :class="[{ 'overflow-hidden': isShowSearchModal || isOpenMenu }, { 'main-bg': !isLoading }]">
     <AppPageLoading />
-    <div class="flex flex-col min-h-screen" :class="isLoading
-      ? 'opacity-0 pointer-events-none'
-      : 'opacity-100 pointer-events-auto'">
-      <AppHeader />
-      <main class="lg:flex-1 mt-24">
-        <NuxtPage />
-      </main>
-      <AppFooter />
-    </div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </Body>
 </template>
