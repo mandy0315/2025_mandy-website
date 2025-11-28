7b927b908b23f654940deade0a97dd75cc9f43b7
mandy <mandy3152618@gmail.com>
2025-11-28 11:35:24 +0800

    fix:作品頁面修正中

--- app/pages/works/index.vue
+++ app/pages/works/index.vue
@@ -1,3 +1,13 @@
+<script setup lang="ts">
+const route = useRoute();
+usePageSEO({
+  title: '作品',
+  path: route.path,
+})
+</script>
 <template>
-  <div>test</div>
+  <div>
+    <h1 class="text-4xl font-bold mb-6">作品集</h1>
+    <p>作品內容更動中...</p>
+  </div>
 </template>
