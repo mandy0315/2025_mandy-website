d7bf54e2717bf7a7a039ee5934697d4896508977
mandy <mandy3152618@gmail.com>
2026-01-23 17:08:35 +0800

    fix: 分類在資料端處理客戶端與伺服器端資料一致

--- app/composables/useCategory.ts
+++ app/composables/useCategory.ts
@@ -2,26 +2,24 @@ export const useCategory = async (
   collection: "blog" | "notes" = "blog",
   limit?: number,
 ) => {
-  const { data, refresh } = await useAsyncData(
+  const { data: categories, refresh } = await useAsyncData(
     `category-${collection}-data`,
-    () => {
-      return queryCollection(collection)
+    async () => {
+      const data = await queryCollection(collection)
         .order("date", "DESC")
         .select("category")
         .all();
+      if (!data) return [];
+
+      // 處理重複與限制數量
+      const categories = data.map((item) => item.category);
+      const uniqueCategories = Array.from(new Set(categories));
+      if (limit) {
+        return uniqueCategories.slice(0, limit);
+      }
+      return uniqueCategories;
     },
   );
-
-  const categories = computed(() => {
-    if (!data.value) return [];
-    const categories = data.value.map((item) => item.category);
-    const uniqueCategories = Array.from(new Set(categories));
-
-    if (limit) {
-      return uniqueCategories.slice(0, limit);
-    }
-    return uniqueCategories;
-  });
 
   const refreshCategories = async () => {
     await refresh();
