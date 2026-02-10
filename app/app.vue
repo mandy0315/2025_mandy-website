248e3dd66e432375b6271cdbd9ef0c68a5517f8d
Mandy Tsai <41838993+mandy0315@users.noreply.github.com>
2026-02-10 19:03:55 +0800

    Revert "fix: isOpenImgModal 用 state 管理讓值同步"

--- app/app.vue
+++ app/app.vue
@@ -1,13 +1,10 @@
 <script setup lang="ts">
 const { isShowSearchModal } = await useSearch();
 const isOpenMenu = useState('isOpenMenu', () => false);
-const isOpenImgModal = useState<boolean>('isOpenImgModal', () => false);
-
-const isLocked = computed(() => isShowSearchModal.value || isOpenMenu.value || isOpenImgModal.value);
 </script>
 
 <template>
-  <Body :class="{ 'overflow-hidden': isLocked }">
+  <Body :class="{ 'overflow-hidden': isShowSearchModal || isOpenMenu }">
     <AppPageLoading />
     <NuxtLayout>
       <NuxtPage />
