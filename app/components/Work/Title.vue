a57e55829ff19388e8f79452620ba90b15b45e64
mandy <mandy3152618@gmail.com>
2026-02-11 11:51:32 +0800

    style: 修改文字主色與反轉色命名

--- app/components/Work/Title.vue
+++ app/components/Work/Title.vue
@@ -5,7 +5,7 @@ const props = defineProps<{
 }>()
 </script>
 <template>
-  <h2 class="font-bold font-zen-old-mincho text-lg text-text-light dark:text-text-dark pb-2">
+  <h2 class="font-bold font-zen-old-mincho text-lg text-text-primary dark:text-text-inverse pb-2">
     <span class="pr-1">{{ props.title }}</span>
     <span class="text-base">{{ props.subtitle }}</span>
   </h2>
