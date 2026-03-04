d1b1529fb6cf2589238f69d7465f0fcafb3abb96
mandy <mandy3152618@gmail.com>
2026-03-04 15:58:08 +0800

    chore: 標籤與類別列表使用 section 與 article

--- app/components/Post/Tags/List.vue
+++ app/components/Post/Tags/List.vue
@@ -18,9 +18,9 @@ const { posts, currentPage, currentSort } = await useArchivePosts({
 
 </script>
 <template>
-  <div>
+  <article>
     <!-- title -->
-    <div class="pb-4">
+    <section class="pb-4">
       <div class="lg:text-sm text-center font-bold font-zen-old-mincho pb-2">
         <span class="px-1">-</span>
         <BaseLink variant="underline" :to="collection === 'blog' ? '/blog' : '/notes'" class="pr-2">{{ collection ===
@@ -38,22 +38,23 @@ const { posts, currentPage, currentSort } = await useArchivePosts({
         <span class="text-primary font-medium">{{ posts.totalPosts || 0 }}</span>
         篇
       </p>
-    </div>
+    </section>
 
-    <BaseSelect class="pb-4" v-model="currentSort" labelTitle="文章排序:"
-      :options="[{ label: '新到舊', value: 'DESC' }, { label: '舊到新', value: 'ASC' }]" />
+    <section>
+      <BaseSelect class="pb-4" v-model="currentSort" labelTitle="文章排序:"
+        :options="[{ label: '新到舊', value: 'DESC' }, { label: '舊到新', value: 'ASC' }]" />
+      <!-- content -->
+      <div class="pb-20 lg:pb-0">
+        <div v-if="collection === 'blog'" class="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          <PostCard v-for="post in posts.list" v-bind="post" :key="post.title" class="col-span-1" />
+        </div>
+        <div v-if="collection === 'notes'" class="border-t border-b border-secondary/50 dark:border-secondary-light/50">
+          <PostItem v-for="post in posts.list" v-bind="post" :key="post.title"
+            class="border-b border-secondary/50 dark:border-secondary-light/50 last:border-0" />
+        </div>
 
-    <!-- content -->
-    <div class="pb-20 lg:pb-0">
-      <div v-if="collection === 'blog'" class="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-        <PostCard v-for="post in posts.list" v-bind="post" :key="post.title" class="col-span-1" />
+        <BasePagination v-if="posts.totalPage" v-model:current-page="currentPage" :totalPage="posts.totalPage" />
       </div>
-      <div v-if="collection === 'notes'" class="border-t border-b border-secondary/50 dark:border-secondary-light/50">
-        <PostItem v-for="post in posts.list" v-bind="post" :key="post.title"
-          class="border-b border-secondary/50 dark:border-secondary-light/50 last:border-0" />
-      </div>
-
-      <BasePagination v-if="posts.totalPage" v-model:current-page="currentPage" :totalPage="posts.totalPage" />
-    </div>
-  </div>
+    </section>
+  </article>
 </template>
