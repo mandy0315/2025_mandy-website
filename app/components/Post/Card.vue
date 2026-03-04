87d501f73a32d065ec9be6afb5b63468d3209187
mandy <mandy3152618@gmail.com>
2026-03-04 15:54:11 +0800

    chore: 文章列表使用 section 與 article

--- app/components/Post/Card.vue
+++ app/components/Post/Card.vue
@@ -17,26 +17,28 @@ const processedSrc = computed(() => getAssetPath(props.image));
 </script>
 
 <template>
-  <NuxtLink :to="path" class="group">
-    <div class="grid grid-cols-1" :class="isHorizontal ? 'lg:grid-cols-7' : ''">
+  <article>
+    <NuxtLink :to="path" class="group block">
+      <div class="grid grid-cols-1" :class="isHorizontal ? 'lg:grid-cols-7' : ''">
 
-      <!-- 圖片比例 16:9 -->
-      <div class="w-full h-auto aspect-video relative rounded-lg overflow-hidden col-span-1"
-        :class="isHorizontal ? 'lg:col-span-3' : ''">
-        <BaseHoverMask contentText="進入文章" />
-        <span class="absolute top-2 z-10 text-xs bg-primary-dark px-1 text-white">{{ category }}</span>
-        <img :src="processedSrc" :alt="title"
-          class="h-full transition inset-0 object-center object-cover w-full absolute duration-500 ease-in-out group-hover:blur-sm" />
-      </div>
+        <!-- 圖片比例 16:9 -->
+        <div class="w-full h-auto aspect-video relative rounded-lg overflow-hidden col-span-1"
+          :class="isHorizontal ? 'lg:col-span-3' : ''">
+          <BaseHoverMask contentText="進入文章" />
+          <span class="absolute top-2 z-10 text-xs bg-primary-dark px-1 text-white">{{ category }}</span>
+          <img :src="processedSrc" :alt="title"
+            class="h-full transition inset-0 object-center object-cover w-full absolute duration-500 ease-in-out group-hover:blur-sm" />
+        </div>
 
-      <!-- 內容區 -->
-      <div class="p-2 col-span-1" :class="isHorizontal ? 'lg:flex lg:flex-col lg:px-6 lg:py-4 lg:col-span-4' : ''">
-        <PostDate :date />
-        <p class="text-xl"> {{ title }}</p>
-        <p class="text-muted dark:text-inverse line-clamp-2" :class="isHorizontal ? 'lg:line-clamp-3' : ''">
-          {{ description }}
-        </p>
+        <!-- 內容區 -->
+        <div class="p-2 col-span-1" :class="isHorizontal ? 'lg:flex lg:flex-col lg:px-6 lg:py-4 lg:col-span-4' : ''">
+          <PostDate :date />
+          <p class="text-xl"> {{ title }}</p>
+          <p class="text-muted dark:text-inverse line-clamp-2" :class="isHorizontal ? 'lg:line-clamp-3' : ''">
+            {{ description }}
+          </p>
+        </div>
       </div>
-    </div>
-  </NuxtLink>
+    </NuxtLink>
+  </article>
 </template>
--- app/components/Post/List/index.vue
+++ app/components/Post/List/index.vue
@@ -8,8 +8,8 @@ const postIndex = computed(() => currentPage.value === 1 ? 1 : 0);
 
 </script>
 <template>
-  <div class="pb-10">
-    <div class="pb-6">
+  <article class="pb-10">
+    <section class="pb-6">
       <BaseTitle>
         <span v-if="collection === 'blog'">部落格</span>
         <span v-else>筆記</span>
@@ -19,25 +19,24 @@ const postIndex = computed(() => currentPage.value === 1 ? 1 : 0);
         <span class="text-primary-dark dark:text-primary font-medium">{{ posts.totalPosts || 0 }}</span>
         篇
       </p>
-    </div>
+    </section>
+    <section>
+      <BaseSelect class="pb-4" v-model="currentSort" labelTitle="文章排序:"
+        :options="[{ label: '新到舊', value: 'DESC' }, { label: '舊到新', value: 'ASC' }]" />
+      <div v-if="collection === 'blog'" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <template v-for="(post, index) in posts.list" :key="post.title">
+          <PostCard v-if="currentPage === 1 && index === 0" v-bind="{ ...post, isHorizontal: true }"
+            class="col-span-1 lg:col-span-full" />
+          <PostCard v-if="index >= postIndex" v-bind="post" class="col-span-1" />
+        </template>
+      </div>
+      <div v-if="collection === 'notes'" class="border-t border-b border-secondary/50 dark:border-secondary-light/50">
+        <PostItem v-for="post in posts.list" v-bind="post" :key="post.title"
+          class="border-b border-secondary/50 dark:border-secondary-light/50 last:border-0" />
+      </div>
 
-    <BaseSelect class="pb-4" v-model="currentSort" labelTitle="文章排序:"
-      :options="[{ label: '新到舊', value: 'DESC' }, { label: '舊到新', value: 'ASC' }]" />
+      <BasePagination v-if="posts.totalPage" v-model:current-page="currentPage" :totalPage="posts.totalPage" />
+    </section>
 
-    <div v-if="collection === 'blog'" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-      <template v-for="(post, index) in posts.list" :key="post.title">
-        <PostCard v-if="currentPage === 1 && index === 0" v-bind="{ ...post, isHorizontal: true }"
-          class="col-span-1 lg:col-span-full" />
-        <PostCard v-if="index >= postIndex" v-bind="post" class="col-span-1" />
-      </template>
-    </div>
-    <div v-if="collection === 'notes'" class="border-t border-b border-secondary/50 dark:border-secondary-light/50">
-      <PostItem v-for="post in posts.list" v-bind="post" :key="post.title"
-        class="border-b border-secondary/50 dark:border-secondary-light/50 last:border-0" />
-    </div>
-
-
-    <BasePagination v-if="posts.totalPage" v-model:current-page="currentPage" :totalPage="posts.totalPage" />
-
-  </div>
+  </article>
 </template>
