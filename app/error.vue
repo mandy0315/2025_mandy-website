8a3802111fc4eed8bbe4cb9ed1869023f9c7fb02
mandy <mandy3152618@gmail.com>
2026-03-14 01:39:56 +0800

    perf: 部落格與其他圖片格式改 webp

--- app/error.vue
+++ app/error.vue
@@ -3,7 +3,7 @@
     <AppHeader />
     <main class="c-container mt-24 flex-1">
       <div class="max-w-2xl mx-auto py-10">
-        <img class="w-full" src="/images/404-twin.png" alt="404圖">
+        <img class="w-full" src="/images/404-twin.webp" alt="404圖">
       </div>
       <h1 class="text-2xl text-muted dark:text-inverse text-center font-zen-old-mincho font-bold pb-1">
         你迷路了嘛～ <span class="block lg:inline">我們回到" <BaseLink variant="underline" to="/">首頁</BaseLink><span
